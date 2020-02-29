d08ad01a81c5ba544aeee2112140e57c3ae0349c
João Pedro Lopes Mendes <joaopedrolopesmendes>
2020-02-29 22:10:04 +0000

    +ADD Added most needed functionaity

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,73 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-array-input'
+import ArrayInput from 'react-array-input'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+import {MDBInput, MDBContainer, MDBBtn} from "mdbreact";
+
 
 export default class App extends Component {
+  state = {
+    array: [
+      {name: '', age: null},
+      {name: '', age: null},
+    ]
+  }
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <MDBContainer style={{width: "50%"}}>
+        <pre>{JSON.stringify(this.state.array)}</pre>
+        <ArrayInput array={this.state.array} onValueChange={(values) => this.setState({array: values})}>
+          {
+            (props) => <Person {...props}/>
+          }
+        </ArrayInput>
+      </MDBContainer>
+
     )
   }
 }
+
+class Person extends Component {
+  state = {
+    name: '',
+    age: 0
+  }
+  componentDidMount() {
+    this.setState(
+      ...this.props.values
+    )
+  }
+  onValueChange = (key, value) => {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  onBlur = () => {
+    const {values} = this.props;
+    this.props.onValuesChange({...values, ...this.state})
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <MDBInput
+          label="Name"
+          value={this.name}
+          onBlur={() => this.onBlur()}
+          onChange={(e) => this.onValueChange('name', e.target.value)}/>
+          
+          <MDBInput
+          label="Age"
+          value={this.age}
+          onBlur={() => this.onBlur()}
+          onChange={(e) => this.onValueChange('age', e.target.value)}/>
+          <MDBBtn onClick={this.props.selfDestroy}>Destroy</MDBBtn>
+
+      </React.Fragment>
+    )
+  }
+}
+
